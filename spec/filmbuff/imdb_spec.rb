fc8528998ecdc4868cfd9d1bad580f4801c4388c
Kristoffer <git@kristoffer.is>
2012-07-05 17:00:55 +0200

    Don't think this is necessary

--- spec/filmbuff/imdb_spec.rb
+++ spec/filmbuff/imdb_spec.rb
@@ -1,27 +1,29 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
 describe FilmBuff::IMDb do
-  before :all do
-    @imdb = FilmBuff::IMDb.new
-  end
-
-  describe "#find_by_id" do
+  context 'given no options' do
     before :all do
-      @title = @imdb.find_by_id("tt0032138")
+      @imdb = FilmBuff::IMDb.new
     end
 
-    it "returns a Title" do
-      @title.instance_of?(FilmBuff::Title).should be_true
-    end
-  end
+    describe "#find_by_id" do
+      before :all do
+        @title = @imdb.find_by_id("tt0032138")
+      end
 
-  describe "#find_by_title" do
-    before :all do
-      @title = @imdb.find_by_title("The Wizard of Oz")
+      it "returns a Title" do
+        @title.instance_of?(FilmBuff::Title).should be_true
+      end
     end
 
-    it "returns a Title" do
-      @title.instance_of?(FilmBuff::Title).should be_true
+    describe "#find_by_title" do
+      before :all do
+        @title = @imdb.find_by_title("The Wizard of Oz")
+      end
+
+      it "returns a Title" do
+        @title.instance_of?(FilmBuff::Title).should be_true
+      end
     end
   end
 end
--- spec/filmbuff/title_spec.rb
+++ spec/filmbuff/title_spec.rb
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 require File.expand_path(File.join(File.dirname(__FILE__), '../spec_helper'))
 
 describe FilmBuff::Title do
