b3818aa2eb1d7f2e852482eb2cb74309564a1b2c
Kristoffer <git@kristoffer.is>
2012-07-05 18:54:10 +0200

    Replace double quotes with single quotes

--- lib/filmbuff/title.rb
+++ lib/filmbuff/title.rb
@@ -4,16 +4,16 @@ module FilmBuff
       :poster_url, :genres, :release_date
 
     def initialize(options = {})
-      @imdb_id = options["tconst"]
-      @title = options["title"]
-      @tagline = options["tagline"]
-      @plot = options["plot"]["outline"] if options["plot"]
-      @runtime = options["runtime"]["time"] if options["runtime"]
-      @rating = options["rating"]
-      @votes = options["num_votes"]
-      @poster_url = options["image"]["url"] if options["image"]
-      @genres = options["genres"] || []
-      @release_date = Date.strptime(options["release_date"]["normal"], '%Y-%m-%d')
+      @imdb_id = options['tconst']
+      @title = options['title']
+      @tagline = options['tagline']
+      @plot = options['plot']['outline'] if options['plot']
+      @runtime = options['runtime']['time'] if options['runtime']
+      @rating = options['rating']
+      @votes = options['num_votes']
+      @poster_url = options['image']['url'] if options['image']
+      @genres = options['genres'] || []
+      @release_date = Date.strptime(options['release_date']['normal'], '%Y-%m-%d')
     end
   end
 end
