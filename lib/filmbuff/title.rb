dd0b5ef759b798f6dc680d909b15cf3fe1118171
Kristoffer <git@kristoffer.is>
2012-07-12 15:02:51 +0200

    Only set release date and tagline if they can be retrieved from IMDb

--- lib/filmbuff/title.rb
+++ lib/filmbuff/title.rb
@@ -6,14 +6,18 @@ module FilmBuff
     def initialize(options = {})
       @imdb_id = options['tconst']
       @title = options['title']
-      @tagline = options['tagline']
+      @tagline = options['tagline'] if options['tagline']
       @plot = options['plot']['outline'] if options['plot']
       @runtime = options['runtime']['time'] if options['runtime']
       @rating = options['rating']
       @votes = options['num_votes']
       @poster_url = options['image']['url'] if options['image']
       @genres = options['genres'] || []
-      @release_date = Date.strptime(options['release_date']['normal'], '%Y-%m-%d')
+
+      if options['release_date']
+        @release_date = Date.strptime(options['release_date']['normal'],
+                                      '%Y-%m-%d')
+      end
     end
   end
 end
