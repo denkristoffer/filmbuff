6e7f61897e0f4f3694c8420df2529144f94c0911
Kristoffer <git@kristoffer.is>
2012-07-12 15:34:13 +0200

    Test more

--- test/filmbuff/test_filmbuff_imdb.rb
+++ test/filmbuff/test_filmbuff_imdb.rb
@@ -16,12 +16,35 @@ describe FilmBuff::IMDb do
   end
 
   describe '#find_by_title' do
-    before do
-      @title = @imdb.find_by_title('The Wizard of Oz')
+    describe 'with default options' do
+      before do
+        @title = @imdb.find_by_title('The Wizard of Oz')
+      end
+
+      it 'returns an array of titles' do
+        assert_instance_of(Array, @title)
+      end
     end
 
-    it 'returns an array of titles' do
-      assert_instance_of(Array, @title)
+    describe 'given a limit of 3' do
+      before do
+        @title = @imdb.find_by_title('The Wizard of Oz', limit: 3)
+      end
+
+      it 'returns 3 results' do
+        assert_equal(3, @title.size)
+      end
+    end
+
+    describe 'when only returning popular titles' do
+      before do
+        @title = @imdb.find_by_title('The Wizard of Oz',
+                                     types: %w(title_popular))
+      end
+
+      it 'returns the 1939 version' do
+        assert_equal('1939', @title.first[:release_year])
+      end
     end
   end
 end
