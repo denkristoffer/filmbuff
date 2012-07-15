8358a4231656bc32beaaf85d1a7ccd13d0a6d9b9
Kristoffer <git@kristoffer.is>
2012-07-15 16:36:54 +0200

    Remove parentheses in tests

--- test/filmbuff/test_filmbuff_title.rb
+++ test/filmbuff/test_filmbuff_title.rb
@@ -11,46 +11,45 @@ describe FilmBuff::Title do
     end
 
     it 'has an IMDb ID' do
-      assert_equal('tt0032138', @title.imdb_id)
+      assert_equal 'tt0032138', @title.imdb_id
     end
 
     it 'has a title' do
-      assert_equal('The Wizard of Oz', @title.title)
+      assert_equal 'The Wizard of Oz', @title.title
     end
 
     it 'has a tagline' do
-      assert_equal('Mighty Miracle Show Of 1000 Delights !', @title.tagline)
+      assert_equal 'Mighty Miracle Show Of 1000 Delights !', @title.tagline
     end
 
     it 'has a plot' do
-      assert_equal('Dorothy Gale is swept away to a magical land in ' <<
+      assert_equal 'Dorothy Gale is swept away to a magical land in ' <<
         'a tornado and embarks on a quest to see the Wizard who can help her ' <<
-        'return home.', @title.plot)
+        'return home.', @title.plot
     end
 
     it 'has a runtime' do
-      assert_equal(6060, @title.runtime)
+      assert_equal 6060, @title.runtime
     end
 
     it 'has a rating' do
-      assert_instance_of(Float, @title.rating)
+      assert_instance_of Float, @title.rating
     end
 
     it 'has an amount of votes' do
-      assert_instance_of(Fixnum, @title.votes)
+      assert_instance_of Fixnum, @title.votes
     end
 
     it 'has a poster URL' do
-      assert_match(%r{\Ahttp://ia.media-imdb.com/images/.*/}, @title.poster_url)
+      assert_match %r{\Ahttp://ia.media-imdb.com/images/.*/}, @title.poster_url
     end
 
     it 'has genres' do
-      assert_equal(%w(Adventure Family Fantasy Musical), @title.genres)
+      assert_equal %w(Adventure Family Fantasy Musical), @title.genres
     end
 
     it 'has a release date' do
-      assert_equal(Date.strptime('1939-08-25', '%Y-%m-%d'),
-        @title.release_date)
+      assert_equal Date.strptime('1939-08-25', '%Y-%m-%d'), @title.release_date
     end
   end
 
@@ -62,7 +61,7 @@ describe FilmBuff::Title do
       end
 
       it 'returns German information' do
-        assert_equal('Das zauberhafte Land', @title.title)
+        assert_equal 'Das zauberhafte Land', @title.title
       end
     end
 
@@ -73,7 +72,7 @@ describe FilmBuff::Title do
       end
 
       it 'returns English information' do
-        assert_equal('The Wizard of Oz', @title.title)
+        assert_equal 'The Wizard of Oz', @title.title
       end
     end
 
@@ -84,7 +83,7 @@ describe FilmBuff::Title do
       end
 
       it 'returns Spanish information' do
-        assert_equal('El mago de Oz', @title.title)
+        assert_equal 'El mago de Oz', @title.title
       end
     end
 
@@ -95,7 +94,7 @@ describe FilmBuff::Title do
       end
 
       it 'returns French information' do
-        assert_equal('Le magicien d\'Oz', @title.title)
+        assert_equal 'Le magicien d\'Oz', @title.title
       end
     end
 
@@ -106,7 +105,7 @@ describe FilmBuff::Title do
       end
 
       it 'returns Italian information' do
-        assert_equal('Il mago di Oz', @title.title)
+        assert_equal 'Il mago di Oz', @title.title
       end
     end
 
@@ -117,7 +116,7 @@ describe FilmBuff::Title do
       end
 
       it 'returns Portugese information' do
-        assert_equal('O Feiticeiro de Oz', @title.title)
+        assert_equal 'O Feiticeiro de Oz', @title.title
       end
     end
   end
