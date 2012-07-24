125ca373146ae8c7c567851a6572d8395344690b
Kristoffer <hello@kristoffer.is>
2012-07-24 21:16:40 +0200

    Relative file requiring

--- lib/filmbuff.rb
+++ lib/filmbuff.rb
@@ -1,6 +1,5 @@
 require 'httparty'
 require 'httparty-icebox'
 
-dir = File.expand_path(File.dirname(__FILE__))
-require File.join(dir, 'filmbuff', 'imdb')
-require File.join(dir, 'filmbuff', 'title')
+require 'filmbuff/imdb'
+require 'filmbuff/title'
