1d437288e7ec5ea48566ab769efdda7611106a94
Kristoffer Sachse <hello@kristoffer.is>
2012-07-28 17:19:17 +0200

    Add YARD documentation

--- lib/filmbuff/title.rb
+++ lib/filmbuff/title.rb
@@ -1,21 +1,50 @@
 module FilmBuff
+  # Contains all accessible information on an IMDb title
   class Title
-    attr_reader :imdb_id, :title, :tagline, :plot, :runtime, :rating, :votes,
-                :poster_url, :genres, :release_date
+    # @return [String] Returns the IMDb ID of `Title`
+    attr_reader :imdb_id
 
-    def initialize(options = {})
-      @imdb_id = options['tconst']
-      @title = options['title']
-      @tagline = options['tagline'] if options['tagline']
-      @plot = options['plot']['outline'] if options['plot']
-      @runtime = options['runtime']['time'] if options['runtime']
-      @rating = options['rating']
-      @votes = options['num_votes']
-      @poster_url = options['image']['url'] if options['image']
-      @genres = options['genres'] || []
+    # @return [String] Returns the title of `Title`
+    attr_reader :title
 
-      if options['release_date']
-        @release_date = Date.strptime(options['release_date']['normal'],
+    # @return [String] Returns the tagline of `Title`
+    attr_reader :tagline
+
+    # @return [String] Returns the plot summary of `Title`
+    attr_reader :plot
+
+    # @return [Integer] Returns the runtime of `Title` in seconds
+    attr_reader :runtime
+
+    # @return [Float] Returns the IMDb rating of `Title`
+    attr_reader :rating
+
+    # @return [Integer] Returns the amount of votes that have been used to
+    #   determine the rating of `Title`
+    attr_reader :votes
+
+    # @return [String] Returns a URL for the poster of `Title`
+    attr_reader :poster_url
+
+    # @return [Array<String>] Returns the genres of `Title`
+    attr_reader :genres
+
+    # @return [Date] Returns the release date of `Title`
+    attr_reader :release_date
+
+    def initialize(imdb_hash = {})
+      @imdb_id = imdb_hash['tconst']
+      @title = imdb_hash['title']
+      @tagline = imdb_hash['tagline'] if imdb_hash['tagline']
+      @plot = imdb_hash['plot']['outline'] if imdb_hash['plot']
+      @runtime = imdb_hash['runtime']['time'] if imdb_hash['runtime']
+      @rating = imdb_hash['rating']
+      @votes = imdb_hash['num_votes']
+      @poster_url = imdb_hash['image']['url'] if imdb_hash['image']
+      @genres = imdb_hash['genres'] || []
+
+      if imdb_hash['release_date']
+        @release_date = Date.strptime(imdb_hash['release_date']['normal'],
                                       '%Y-%m-%d')
       end
     end
