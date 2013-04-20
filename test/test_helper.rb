16e15491a8282b53444777bd0b8e8cc2ba79194e
Kristoffer Sachse <hello@kristoffer.is>
2013-04-20 15:17:35 +0200

    Add Coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 require 'bundler/setup'
 require 'minitest/autorun'
+
+if ENV['TRAVIS'] == 'true'
+  require 'coveralls'
+  Coveralls.wear!
+end
+
 require 'vcr'
 
 require_relative '../lib/filmbuff'
